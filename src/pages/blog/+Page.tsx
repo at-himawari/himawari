2fd5037b4a547417817e98c6903c6fbbb2b8df62
himawari-aerobytes <himawari-aerobytes@users.noreply.github.com>
2025-07-24 15:03:13 +0900

    [ADD] +Page.tsx

--- src/pages/blog/+Page.tsx
+++ src/pages/blog/+Page.tsx
@@ -24,10 +24,10 @@ export default function Page() {
     <>
       <Header />
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold mb-12 text-left text-gray-800">
+        <h1 className="text-4xl font-bold text-left text-orange-600 title-ja">
           ブログ
         </h1>
-        <h2 className="text-2xl font-bold mb-12 text-left text-gray-800">
+        <h2 className="text-2xl font-bold mb-12 text-left text-gray-800 section-h2">
           Blog
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">

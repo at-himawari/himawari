7a70c8f99e665b87732e6f7b5ce5a4df49c86fe6
himawari-aerobytes <himawari-aerobytes@users.noreply.github.com>
2025-07-24 22:46:17 +0900

    [ADD] Metaを追加

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -12,5 +12,13 @@ export default {
   favicon,
   title: "Himawari Project",
   description: "映像とITの融合領域にチャレンジするプロジェクト",
-  image:HimawariIcon
+  image: HimawariIcon,
+  meta: {
+    "og:image": HimawariIcon,
+    "og:type": "website",
+    "twitter:card": "summary_large_image",
+    "twitter:title": "Himawari Project",
+    "twitter:description": "映像とITの融合領域にチャレンジするプロジェクト",
+    "twitter:image": HimawariIcon,
+  },
 };

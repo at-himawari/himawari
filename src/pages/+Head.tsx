8776d6c2ac15c263a999d36d174615519c5dcdff
himawari-aerobytes <himawari-aerobytes@users.noreply.github.com>
2025-07-24 23:18:50 +0900

    [FIX] Headの修正

--- src/pages/+Head.tsx
+++ src/pages/+Head.tsx
@@ -1,5 +1,7 @@
-import HimawariIcon from "../../public/himawari.png";
 export function Head() {
+  const siteUrl = "https://at-himawari.com";
+  const imageUrl = `${siteUrl}/himawari.png`;
+
   return (
     <>
       <meta property="og:type" content="article" />
@@ -7,7 +9,7 @@ export function Head() {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Himawari Project" />
       <meta name="twitter:description" content="ときどき仕事して、休んで" />
-      <meta name="twitter:image" content={HimawariIcon} />
+      <meta name="twitter:image" content={imageUrl} />
       <meta name="twitter:site" content="@at_himawari" />
     </>
   );

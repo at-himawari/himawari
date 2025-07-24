7885e8aa972687b35478b4804d8739df7bf3e870
himawari-aerobytes <himawari-aerobytes@users.noreply.github.com>
2025-07-24 23:33:08 +0900

    [FIX] 実験的に変えてみる

--- src/pages/+Head.tsx
+++ src/pages/+Head.tsx
@@ -9,7 +9,7 @@ export function Head() {
       {/* Twitter Card Tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Himawari Project" />
-      <meta name="twitter:description" content="ときどき仕事して、休んで" />
+      <meta name="twitter:description" content="のんびり仕事する" />
       <meta name="twitter:image" content={imageUrl} />
       <meta name="twitter:site" content="@at_himawari" />
     </>

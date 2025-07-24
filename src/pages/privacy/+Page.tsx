7bfbbbca37893a7d4b06c0700f84fb5b7184217a
himawari-aerobytes <himawari-aerobytes@users.noreply.github.com>
2025-07-24 21:29:54 +0900

    [FIX] 不要な宣言削除

--- src/pages/privacy/+Page.tsx
+++ src/pages/privacy/+Page.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import ReactMarkdown from "react-markdown";

94e73dc1373022624a5ff4c344578a870394cf63
羽ばたくエンジニア <at-himawari@users.noreply.github.com>
2025-09-21 16:36:12 +0900

    [UPDATE] テスト設定の修正

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],

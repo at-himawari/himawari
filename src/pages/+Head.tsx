6c0cbf67547b0530918109c15da8c0e8166773f3
羽ばたくエンジニア <at-himawari@users.noreply.github.com>
2025-09-25 16:43:05 +0900

    [ADD] Microsoft Clarity導入

--- src/pages/+Head.tsx
+++ src/pages/+Head.tsx
@@ -36,6 +36,19 @@ export function Head() {
 
   return (
     <>
+      {/** Microsoft Clarity */}
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tg68brif3l");
+        `,
+        }}
+      />
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6651283997191475"

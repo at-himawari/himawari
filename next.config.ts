4ce8c21819844668f8198e47f85276468b930915
羽ばたくエンジニア <at-himawari@users.noreply.github.com>
2026-06-27 12:58:33 +0900

    Disable trailing slash redirects

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.resolve(__dirname),
+  skipTrailingSlashRedirect: true,
   images: {
     unoptimized: true,
   },
